60308a73b49ba0f3a72f7f0fe69ecaab0b309375
lcranfrd <lcranfrd@comcast.net>
2021-02-18 13:08:20 -0500

    Update functionality

--- js/scripts.js
+++ js/scripts.js
@@ -31,39 +31,33 @@ getUsers(employeesUrl)
     data.forEach((v) => liveIds.push(v.login.uuid));
     makeEmployees(data,liveIds);
     createModalBones(data);
+    addSearch(data, liveIds);
   })
   .catch((e) => console.error(`Error of ${e}`));
 
-function makeEmployees(data, liveIds) {
-  console.log(data);
-  data.forEach((v) => {
-    const employeeHtml = `
-      <div id="${v.login.uuid}" class="card">
-      <div class="card-img-container">
-          <img class="card-img" src="${v.picture.large}" alt="profile picture">
+function makeEmployees(data) {
+  data.filter((v1) => liveIds.includes(v1.login.uuid))
+    .forEach((v) => {
+      const employeeHtml = `
+        <div id="${v.login.uuid}" class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${v.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${v.name.first} ${v.name.last}</h3>
+            <p class="card-text">${v.email}</p>
+            <p class="card-text cap">${v.location.city}, ${v.location.state}</p>
+        </div>
       </div>
-      <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${v.name.first} ${v.name.last}</h3>
-          <p class="card-text">${v.email}</p>
-          <p class="card-text cap">${v.location.city}, ${v.location.state}</p>
-      </div>
-    </div>
-    `;
-    employeesDiv.insertAdjacentHTML('beforeend', employeeHtml);
-  });
+      `;
+      employeesDiv.insertAdjacentHTML('beforeend', employeeHtml);
+    });
 
   employeesDiv.addEventListener('click', (e) => {
     (e.target.className !== 'gallery') && makeModal(e,data,liveIds);
   });
 }
 
-const searchHtml = `
-<form action="#" method="get">
-  <input type="search" id="search-input" class="search-input" placeholder="Search...">
-  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-</form>
-`;
-searchDiv.insertAdjacentHTML('beforeend', searchHtml);
 
 function createModalBones(data) {
   modalContainerDiv.className = 'modal-container';
@@ -83,74 +77,48 @@ function createModalBones(data) {
   modalContainerDiv.innerHTML = modalHtml;
   modalContainerDiv.style.display  = 'none';
   document.body.insertAdjacentElement('beforeend', modalContainerDiv);
+  
   document.querySelector('#modal-close-btn')
-    .addEventListener('click', () => modalContainerDiv.style.display = 'none');
-
+  .addEventListener('click', () => modalContainerDiv.style.display = 'none');
+  
   document.querySelector('#modal-next').addEventListener('click', (e) => {
     const currentLiveId = document.querySelector('.modal-info-container').id;
-    getEmployee(e, data, liveIds, currentLiveId);
+    getModalEmployee(e, data, currentLiveId);
   });
-
-
+  
   document.querySelector('#modal-prev').addEventListener('click', (e) => {
     const currentLiveId = document.querySelector('.modal-info-container').id;
-    console.log(currentLiveId)
-    getEmployee(e, data, liveIds, currentLiveId);
+    getModalEmployee(e, data, currentLiveId);
   });
 }
 
-function getEmployee(e, data, liveIds, currentLiveId) {
+function getModalEmployee(e, data, currentLiveId) {
+  console.log(liveIds.length)
   const btn = e.target;
   let currentLiveIndx = liveIds.indexOf(currentLiveId);
-  // console.log(`${liveIds} ${currentLiveId} ${currentLiveIndx}`)
   if(btn.id === 'modal-prev') {
-    // currentLiveIndx--;
-    (currentLiveIndx === 0) && (btn.disabled = true);
     document.querySelector('#modal-next').disabled = false;
-    (!btn.disabled) && modalHtml(data[currentLiveIndx-1]);
+    (!btn.disabled) && modalHtml(data[--currentLiveIndx]);
+    (currentLiveIndx === 0) && (btn.disabled = true);
   }
   if(btn.id === 'modal-next') {
-    // currentLiveIndx++;
-    (currentLiveIndx === data.length -1) && (btn.disabled = true);
     document.querySelector('#modal-prev').disabled = false;
-    (!btn.disabled) && modalHtml(data[currentLiveIndx+1]);
+    (!btn.disabled) && modalHtml(data[++currentLiveIndx]);
+    (currentLiveIndx === data.length -1) && (btn.disabled = true);
   }
-
-
+  
+  
 }
 
-function getNextEmployee(e, data, liveIds, currentLiveId) {
-  const nextBtn = document.querySelector('#modal-next');
-  const currentLiveIndx = liveIds.indexOf(currentLiveId);
-  nextBtn.disabled = (currentLiveIndx === data.length -1)? true: false;
-  currentLiveIndx <= data.length -1 && modalHtml(data[currentLiveIndx + 1])
-}
-
-function getPrevEmployee(e, data, liveIds, currentLiveId) {
-  console.log(e.target.id)
-  const prevBtn = document.querySelector('#modal-prev');
-  const currentLiveIndx = liveIds.indexOf(currentLiveId);
-  prevBtn.disabled = (currentLiveIndx -1 === 0)? true: false;
-  console.log(prevBtn);
-  console.log(`${liveIds} ${currentLiveIndx} ${currentLiveIndx === 0}`)
-  currentLiveIndx <= data.length -1 && modalHtml(data[currentLiveIndx - 1])
-}
-
-// function makeModalIds(data, liveIds) {
-//   if(liveIds.length !== 0) {
-//     return data.filter((v) => uuids.includes(v.login.uuid));
-//   }
-// }
-
-function makeModal(e, data, liveIds) {
+function makeModal(e, data) {
   const currentUuid = e.target.closest('.card').id;
   // const currentLiveId = liveIds.indexOf(currentUuid);
   const employee = data.find((v) => currentUuid.includes(v.login.uuid));
   // 23 Portland Ave., Portland, OR 97204
   modalHtml(employee);
-
-}
   
+}
+
 function modalHtml(employee) {
   const modalInfoContainerDiv = document.querySelector('.modal-info-container');
   modalInfoContainerDiv.id = employee.login.uuid;
@@ -167,4 +135,52 @@ function modalHtml(employee) {
   `;
   modalInfoContainerDiv.insertAdjacentHTML('beforeend', modalHtml);
   modalContainerDiv.style.display = 'block';
+}
+
+function clearEmployeesDivMakeLiveIds(data, filteredData) {
+  employeesDiv.innerHTML = '';
+  liveIds = [];
+  (filteredData.length === 0)
+    ? data.forEach((v) => liveIds.push(v.login.uuid))
+    : filteredData.forEach((v) => liveIds.push(v.login.uuid));
+  console.log(liveIds)
+  makeEmployees(data);
+}
+
+function addSearch(data) {
+  const searchHtml = `
+  <form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>
+  `;
+  searchDiv.insertAdjacentHTML('beforeend', searchHtml);
+
+  function execSearch(e, data) {
+    e.preventDefault();
+    const search = document.querySelector('#search-input').value;
+    let filteredData = [];
+    if(search.length > 0) {
+      filteredData = data.filter((v) => 
+        `${v.name.first} ${v.name.last}`.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+    if(filteredData.length > 0) {
+      clearEmployeesDivMakeLiveIds(data, filteredData);
+    } else if(search.length === 0) {
+        clearEmployeesDivMakeLiveIds(data, filteredData);
+      } else{
+          employeesDiv.innerHTML = '';
+          employeesDiv.insertAdjacentHTML('beforeend', `
+          <h1 class="no-results">No Search Results Found</h1>`);
+        }
+    // search.length === 0 && clearEmployeesDivMakeLiveIds(data, liveIds, filteredData);
+  } 
+
+  document.querySelector('#search-input').addEventListener('keyup', (e) => {
+    execSearch(e, data)
+  });
+  document.querySelector('form').addEventListener('submit', (e) => {
+    execSearch(e, data)
+  });
 }
